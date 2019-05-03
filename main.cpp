2151f6f79e6c3ef1047bd80dbe02183b1f76bd7a
Pradyumna Rahul K <prkinformed@gmail.com>
2019-05-03 19:00:29 +0530

    Cleaned up Main

--- main.cpp
+++ main.cpp
@@ -106,45 +106,16 @@ void parseFiles(const char *dirPath) {
 
 int main(int argc, char *argv[])
 {
-    Mat img = imread("/home/inf0rmed/uspProject/numberPlate/test5.jpg", cv::IMREAD_COLOR);
 
-    parseFiles(".");
+    char path[2048];
 
-    return 0;
+    std::cout << "Enter the path to the files: ";
+    std::cin >> path;
 
-    if(img.empty())
-       return -1;
-
-    NumberPlateLocator nbp = NumberPlateLocator(img);
-    // Get the threshold image
-    Mat img_pre = nbp.generateThresholdImage(img);
-    imshow("Threshold Image", img_pre);
-    //waitKey(0);
-    // get the  contours
-    std::vector<std::vector<Point>> cont = nbp.extractContours(img_pre);
-
-    Mat tmp;
-    img.copyTo(tmp);
-    drawContours(tmp, cont, -1, Scalar(255,0,0), 1);
-
-    imshow("Contours", tmp);
-    //waitKey(0);
-
-    // Get the number plate
-    Plate pobj = nbp.cleanAndFindPlate(cont);
-
-    // Perform OCR
-    PlateClassifier plateClassifier = PlateClassifier(pobj.plate, pobj.sub_contours, pobj.r_plate);
-
-    std::string plateData = plateClassifier.recognizePlate(plateClassifier.extractCharacters());
-    std::cout << "Number Plate: " << plateData << "\n";
-    // Display the final extracted plate number
-    putText(tmp, plateData, Point(pobj.r_plate.x, pobj.r_plate.y-10), FONT_HERSHEY_PLAIN, 3.0, Scalar(0, 0, 250), 3);
-    rectangle(tmp, pobj.r_plate, Scalar(0, 255, 0), 2, 8, 0);
-    imshow("Number Plate", tmp);
-    waitKey(0);
+    parseFiles(path);
 
     return 0;
+
 }
 
 
